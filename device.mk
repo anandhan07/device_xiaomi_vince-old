1abb7780f23fdf28da6dde4215e55590cadc1184
GhostMaster69-dev <rathore6375@gmail.com>
2022-07-02 08:57:41 +0530

    vince: Import prebuilt FM Radio packages.
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- device.mk
+++ device.mk
@@ -223,11 +223,6 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1.vendor \
     libunwind
 
-# FM
-PRODUCT_PACKAGES += \
-    FMRadio \
-    libfmjni
-
 # Fwk-detect
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor \
