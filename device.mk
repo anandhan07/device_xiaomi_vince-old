2f92f78824c39cf4372783e824fd4a2753a0cbe0
Pascoato <viniciuspascoato@gmail.com>
2022-09-23 09:50:16 +0530

    vince: Build android.hardware.power@1.2
    - Fixes: I hwservicemanager: getTransport: Cannot find entry android.hardware.power@1.2::IPower/default in either framework or device VINTF manifest.
    
    Signed-off-by: Pascoato <viniciuspascoato@gmail.com>
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- device.mk
+++ device.mk
@@ -380,6 +380,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
+    android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
